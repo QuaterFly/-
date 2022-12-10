8f700fece45ed905eec3bf89574d38c36a7456df
QuaterFly <70107559+QuaterFly@users.noreply.github.com>
2022-12-10 15:57:27 +0300

    Delete lab1/lab1 directory

--- lab1/lab1/Example3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cmath>
-#include <string>
-using namespace std;
-
-void example3(double x, double y, double z) {
-	cout << "y=" << 5. * atan(x) - (1. / 4.) * acos(x) * ((x + 3 * abs(x - y) + pow(x, 2)) / (abs(x - y) * z + pow(x, 2))) << "\n";
-}
-
